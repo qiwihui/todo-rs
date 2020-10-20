58a56082e21a654ad4c1ca88cfeb9504c98f75d7
qiwihui <qwh005007@gmail.com>
2020-10-20 12:42:30 +0800

    feat: add env config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+mod config;
 mod db;
 mod handlers;
 mod models;
 
 use actix_web::{get, web, App, HttpServer, Responder};
-use deadpool_postgres;
+use dotenv::dotenv;
 use handlers::todos;
 use std::io;
 use tokio_postgres::{self, NoTls};
@@ -15,23 +16,20 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    println!("Starting server at http://127.0.0.1:8000");
-
-    let mut cfg = tokio_postgres::Config::new();
-    cfg.host("localhost");
-    cfg.port(5432);
-    cfg.user("actix");
-    cfg.password("actix");
-    cfg.dbname("actix");
-    let mgr = deadpool_postgres::Manager::new(cfg, NoTls);
-    let pool = deadpool_postgres::Pool::new(mgr, 100);
+    dotenv().ok();
+    let cfg = crate::config::Config::from_env().unwrap();
+    let pool = cfg.pg.create_pool(NoTls).unwrap();
+    println!(
+        "Starting server at http://{}:{}",
+        cfg.server.host, cfg.server.port
+    );
     HttpServer::new(move || {
         App::new()
             .data(pool.clone())
             .service(hello)
             .route("/todos{_:/?}", web::get().to(todos))
     })
-    .bind("127.0.0.1:8000")?
+    .bind(format!("{}:{}", cfg.server.host, cfg.server.port))?
     .run()
     .await
 }
