832652377a0da790a245c185a08551e1a7c12fe4
qiwihui <qwh005007@gmail.com>
2020-10-23 14:16:46 +0800

    feat: add log and env log set to info

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ mod db;
 mod handlers;
 mod models;
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpServer, Responder};
 use dotenv::dotenv;
 use handlers::{todo, todos};
+use env_logger;
+use log::info;
 use std::io;
 use tokio_postgres::{self, NoTls};
 
@@ -17,15 +19,20 @@ async fn hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     dotenv().ok();
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
     let cfg = crate::config::Config::from_env().unwrap();
     let pool = cfg.pg.create_pool(NoTls).unwrap();
-    println!(
+    info!(
         "Starting server at http://{}:{}",
         cfg.server.host, cfg.server.port
     );
     HttpServer::new(move || {
         App::new()
             .data(pool.clone())
+            .wrap(middleware::Logger::default())
             .service(hello)
             .route("/todos{_:/?}", web::get().to(todos))
             .route("/todos/{list_id}{_:/?}", web::get().to(todo))
