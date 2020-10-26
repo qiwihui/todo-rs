d21b2edd57a138d6f0d6f0a5a2168d95c3a1f6b5
qiwihui <qwh005007@gmail.com>
2020-10-26 15:19:08 +0800

    test: add get todos test

--- src/tests/handlers.rs
+++ src/tests/handlers.rs
@@ -1,11 +1,32 @@
+use crate::db::create_todo;
 use crate::handlers::CreateTodoList;
 use crate::models;
-use crate::tests::helpers::{assert_get, assert_post};
+use crate::tests::helpers::{assert_get, assert_post, APP_STATE};
 use actix_web::test;
+use deadpool_postgres::Client;
 
 #[actix_rt::test]
 async fn test_hello_world() {
     assert_get("/").await;
+}
+
+#[actix_rt::test]
+async fn test_get_todos() {
+    // create data in db
+    let todo_title = "New Todo List";
+    let client: Client = APP_STATE
+        .pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+    let new_todo = create_todo(&client, todo_title.into()).await;
+    assert!(new_todo.is_ok(), "Failed to create new test todo");
+    // get and check
+    let new_todo = new_todo.unwrap();
+    let response = assert_get("/todos").await;
+    let todos: Vec<models::TodoList> = test::read_body_json(response).await;
+    let maybe_list = todos.iter().find(|todo| todo.id == new_todo.id);
+    assert!(maybe_list.is_some(), "Item not found!");
 }
 
 #[actix_rt::test]
